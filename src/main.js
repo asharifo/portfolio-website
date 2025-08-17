b1b8775e0f22b77c6d756c5867b228eae7cbc6ad
asharifo <asharifo@student.ubc.ca>
2025-08-17 14:42:56 +0700

    Added raycasting, car movement, and billboard clicking events

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 const raycaster = new THREE.Raycaster();
@@ -20,6 +21,13 @@ renderer.shadowMap.enabled = true;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.75;
 
+let car = {
+  instance: null,
+  moveDistance: 1,
+  isMoving: false,
+  moveDuration: 0.2,
+};
+
 const intersectObjects = [];
 const intersectObjectsNames = [
   "billboard1",
@@ -30,7 +38,7 @@ const intersectObjectsNames = [
 
 const Loader = new GLTFLoader();
 Loader.load(
-  "/website-model.glb",
+  "/new-model.glb",
   function (glb) {
     glb.scene.traverse((child) => {
       if (intersectObjectsNames.includes(child.name)) {
@@ -39,6 +47,9 @@ Loader.load(
       if (child.isMesh) {
         child.castShadow = true;
         child.receiveShadow = true;
+      }
+      if (child.name === "Body") {
+        car.instance = child;
       }
     });
     scene.add(glb.scene);
@@ -51,11 +62,17 @@ Loader.load(
 
 const moon = new THREE.DirectionalLight(0xffffff, 1);
 moon.castShadow = true;
-moon.position.set(0, 15, -100);
+moon.position.set(0, 50, -100);
 moon.shadow.mapSize.width = 4096;
 moon.shadow.mapSize.height = 4096;
 moon.shadow.normalBias = 0.1;
+moon.shadow.camera.left = -20
+moon.shadow.camera.right = 20
+moon.shadow.camera.bottom = -10
 scene.add(moon);
+
+const shadowHelper = new THREE.CameraHelper(moon.shadow.camera);
+scene.add(shadowHelper)
 
 const billboardLight1 = new THREE.PointLight(0xffffff, 3, 100);
 billboardLight1.position.set(0.735, 3.74, 0.114);
@@ -95,9 +112,10 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000);
 
-camera.position.x = -0.33523441487767175;
-camera.position.y = 2.1082812638682764;
-camera.position.z = 4.9329414086097865;
+camera.position.x = 0;
+camera.position.y = 2.3;
+camera.position.z = 4.5;
+camera.lookAt(0, 2, 0);
 
 const controls = new OrbitControls(camera, canvas);
 controls.update();
@@ -119,9 +137,52 @@ function onPointerMove(event) {
 function onClick(event) {
 }
 
+function moveCar(targetPosition, cameraPosition) {
+  car.isMoving = true;
+  const t1 = gsap.timeline({
+    onComplete: () => {
+      car.isMoving = false;
+    }
+  });
+  t1.to(car.instance.position, {
+    z: targetPosition.z,
+    duration: car.moveDuration,
+  });
+  t1.to(camera.position, {
+    z: cameraPosition.z,
+    duration: car.moveDuration,
+  }, 0);
+}
+
+function onKeyDown(event) {
+  if (car.isMoving) return; // Prevent movement if already moving
+  const targetPosition = new THREE.Vector3().copy(car.instance.position);
+  const cameraPosition = new THREE.Vector3().copy(camera.position);
+  switch (event.key.toLowerCase()) {
+    case 'w':
+    case 'arrowup':
+      if (car.instance) {
+        targetPosition.z -= car.moveDistance;
+      }
+      cameraPosition.z -= car.moveDistance;
+      break;
+    case 's':
+    case 'arrowdown':
+      if (car.instance) {
+        targetPosition.z += car.moveDistance;
+      }
+      cameraPosition.z += car.moveDistance;
+      break;
+    default:
+      return;
+  }
+  moveCar(targetPosition, cameraPosition);
+}
+
 window.addEventListener('click', onClick);
 window.addEventListener('resize', onResize);
 window.addEventListener('pointermove', onPointerMove);
+window.addEventListener('keydown', onKeyDown);
 
 function animate() {
   // update the picking ray with the camera and pointer position
